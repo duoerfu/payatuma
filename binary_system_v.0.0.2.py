b1a14104a8c620157fac28fe9e8cd64fbcf97359
mavirane <mavirane@github.com>
2019-11-17 15:53:08 +0300

    binary_systemv0.0.1 and v0.0.2 fixed bugs

--- binary_system_v.0.0.2.py
+++ binary_system_v.0.0.2.py
@@ -1,37 +1,46 @@
 # -*- coding: utf-8 -*-
 __title__ = "binary_system"
-__author__ = "Furkan Sari and Ercan Sezdi"
+__author__ = "Furkan Sarı and Ercan Sezdi"
 __version__ = "0.0.2"
 __email__ = "furkansari@mavirane.com and ercansezdizero@gmail.com"
 
 from math import pow
+from matplotlib import pyplot
+import numpy as np
 
 
 class program:
     def __init__(self):
-        print("Program Start!")
-        self.increase = 5
-        self.day = 3
-        self.stage = 3
-        self.people_count = 2
-        self.all_people = 3
-        self.company_all_income = 3000
+        # Loop
+        self.count = int(input('How many loops?\n'))
+        self.stage = 1
+        self.people_count = 1
+        self.company_income = 0
+        self.company_all_income = 0
+        # Plot
+        self.y_datas = []
+        self.figure = pyplot.figure(figsize=(16, 8))
+        self.loop = 1
+        # Start Program
         self.start()
+        self.plot()
 
     def start(self):
-        for i in range(7):
+        for i in range(self.count):
             self.company_income = int(1000 * int(pow(2, (self.stage - 1))))
-            print(self.day, ' gün', self.stage, ' stage')
             self.company_all_income = self.company_income + self.company_all_income
-            print('Company Income', self.company_income)
-            print('Company All Income = ', self.company_all_income)
-            self.all_people = self.people_count + self.all_people
-            self.people_count = int(pow(2, self.stage))
-            print('How many people =', self.all_people)
-            print('------------------------')
-            self.day += self.increase
-            self.increase *= 2
+            print('Stage ', self.stage, ' People Count:', self.people_count, ' Income: ', self.company_income)
+            print('All Income: ', self.company_all_income)
+            # New Stage
             self.stage += 1
+            self.people_count *= 2
+            self.y_datas.append(self.company_income)
+
+    def plot(self):
+        x = np.arange(0, self.count, 1)
+        y = self.y_datas
+        pyplot.plot(x, y, color='red')
+        start_plot = pyplot.show()
 
 
 if __name__ == "__main__":
--- binary_system_v0.0.1.py
+++ binary_system_v0.0.1.py
@@ -9,30 +9,23 @@ from math import pow
 
 class program:
     def __init__(self):
-        print("Program Start!")
-        self.gun = 1
-        self.kademe = 1
+        self.stage = 1
+        self.people_count = 1
+        self.company_income = 0
         self.company_all_income = 0
+        # Start Program
         self.start()
 
     def start(self):
-        for i in range(14):
-            self.company_income = int(1000 * int(pow(2, (self.kademe - 1))))
-            if self.kademe < 7:
-                print(self.gun, ' gün', self.kademe, ' kademe')
-                self.company_all_income = self.company_income + self.company_all_income
-                print(self.company_all_income)
-                self.gun += 1
-                self.kademe += 1
-            if 6 < self.kademe < 11:
-                self.company_income = int(1000 * int(pow(2, (self.kademe - 1))))
-                for j in range(32):
-                    print(self.gun, ' gün', self.kademe, ' kademe')
-                    print(self.company_income, 'company income')
-                    self.company_all_income = int(int(self.company_income / 32) + int(self.company_all_income))
-                    print(self.company_all_income)
-                    self.gun += 1
-                self.kademe += 1
+        count = int(input('How many loops?\n'))
+        for i in range(count):
+            self.company_income = int(1000 * int(pow(2, (self.stage - 1))))
+            self.company_all_income = self.company_income + self.company_all_income
+            print('Stage ', self.stage, ' People Count:', self.people_count, ' Income: ', self.company_income)
+            print('All Income: ', self.company_all_income)
+            # New Stage
+            self.stage += 1
+            self.people_count *= 2
 
 
 if __name__ == "__main__":
